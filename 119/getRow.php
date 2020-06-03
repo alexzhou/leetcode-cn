21a03fc25f9fbc5e519b02ae022cab3d91bebac8
alexzhou1986 <alexzhou1986@gmail.com>
2020-06-03 14:06:43 +0800

    119. 杨辉三角 II

--- 119/getRow.php
+++ 119/getRow.php
@@ -20,6 +20,21 @@ class Solution {
         }
         return $data[$rowIndex+1];
     }
+
+    function getRow2($rowIndex) {
+        if($rowIndex==0)return [1];
+        $preRow = [1];
+        for($i=2;$i<=$rowIndex+1;$i++){
+            $temp = [];
+            $temp[0] = $preRow[0];
+            for ($j=1;$j<$i-1;$j++){
+                $temp[$j] = $preRow[$j-1]+$preRow[$j];
+            }
+            $temp[$i-1] = $preRow[count($preRow)-1];
+            $preRow = $temp;
+        }
+        return $preRow;
+    }
 }
 $obj = new Solution();
 $rowIndex = 3;
