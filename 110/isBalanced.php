dd9a846d7ab3510f1a81ea25df0efe366774cb02
alexzhou1986 <alexzhou1986@gmail.com>
2020-06-04 11:38:26 +0800

    110. 平衡二叉树

---
+++ 110/isBalanced.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+
+class TreeNode {
+    public $val = null;
+    public $left = null;
+    public $right = null;
+    function __construct($value) { $this->val = $value; }
+}
+
+class Solution {
+
+    /**
+     * @param TreeNode $root
+     * @return Boolean
+     */
+    function isBalanced($root) {
+        if ($root == null) return true;
+
+        $lh = $this->height($root->left);
+        $rh = $this->height($root->right);
+
+        if (abs($lh - $rh) <= 1 && $this->isBalanced($root->left) && $this->isBalanced($root->right)){
+            return true;
+        }
+        return false;
+    }
+
+    function height($node){
+        if ($node === null) return 0;
+        return 1 + max($this->height($node->left), $this->height($node->right));
+    }
+}
