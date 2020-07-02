db36c4418feb120ab6be75351574e929204ad1b7
alexzhou1986 <alexzhou1986@gmail.com>
2020-07-02 12:06:46 +0800

    69. x 的平方根

---
+++ src/69/mySqrt.php
@@ -0,0 +1,19 @@
+<?php
+class Solution {
+
+    /**
+     * @param Integer $x
+     * @return Integer
+     */
+    function mySqrt($x) {
+        $g=$x;
+        while(abs($g*$g-$x)>0.1) //小数位数越多精度越高 eg: 0.00001
+        {
+            $g=($g+$x/$g)/2;
+        }
+        return floor($g);
+    }
+}
+$x = 8;
+$obj  = new Solution();
+echo $obj->mySqrt($x);
