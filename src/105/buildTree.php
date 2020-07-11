98dc52d20d70c13ca9c2635d4898bb3d2b2a243f
alexzhou1986 <alexzhou1986@gmail.com>
2020-07-11 14:02:46 +0800

    105

---
+++ src/105/buildTree.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     public $val = null;
+ *     public $left = null;
+ *     public $right = null;
+ *     function __construct($value) { $this->val = $value; }
+ * }
+ */
+class Solution {
+
+    /**
+     * @param Integer[] $preorder
+     * @param Integer[] $inorder
+     * @return TreeNode
+     */
+    function buildTree($preorder, $inorder) {
+        $root = new TreeNode($preorder[0]);
+    }
+}
