85a92078f17b54cb5e225c5f1f5087abbcbb51d4
alexzhou1986 <alexzhou1986@gmail.com>
2020-07-15 17:30:30 +0800

    108. 将有序数组转换为二叉搜索树

---
+++ src/108/sortedArrayToBST.php
@@ -0,0 +1,22 @@
+<?php
+class TreeNode {
+    public $val = null;
+    public $left = null;
+    public $right = null;
+    function __construct($value) { $this->val = $value; }
+}
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return TreeNode
+     */
+    function sortedArrayToBST($nums){
+        if(empty($nums))return null;
+        $pos = count($nums)/2;
+        $node = new TreeNode($nums[$pos]);
+        $node->left = $this->create(array_slice($nums,0,$pos));
+        $node->left = $this->create(array_slice($nums,$pos+1));
+        return $node;
+    }
+}
