a136d8c5e78955c5cc6d536a54ef0369257532d2
alexzhou1986 <alexzhou1986@gmail.com>
2020-09-09 17:57:04 +0800

    commit

---
+++ src/LinkList/合并K个排序链表/mergeKLists.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class ListNode
+{
+    public $val = 0;
+    public $next = null;
+    function __construct($val = 0, $next = null)
+    {
+        $this->val = $val;
+        $this->next = $next;
+    }
+}
+
+class Solution
+{
+
+    /**
+     * @param ListNode[] $lists
+     * @return ListNode
+     */
+    function mergeKLists($lists)
+    {
+        if(count($lists)==0 || (count($lists)==1 && $lists[0]===null))return [];
+        //TODO
+    }
+}
