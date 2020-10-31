26fe013ac0af11763feecb08bdba2ba1e81ef388
alexzhou1986 <alexzhou1986@gmail.com>
2020-10-31 16:50:30 +0800

    commit

---
+++ src/Map/381. O(1) 时间插入、删除和获取随机元素 - 允许重复/RandomizedCollection.go
@@ -0,0 +1,38 @@
+package _81__O_1__时间插入_删除和获取随机元素___允许重复
+
+type RandomizedCollection struct {
+	Values map[int][]int
+}
+
+
+/** Initialize your data structure here. */
+func Constructor() RandomizedCollection {
+
+}
+
+
+/** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+func (this *RandomizedCollection) Insert(val int) bool {
+
+}
+
+
+/** Removes a value from the collection. Returns true if the collection contained the specified element. */
+func (this *RandomizedCollection) Remove(val int) bool {
+
+}
+
+
+/** Get a random element from the collection. */
+func (this *RandomizedCollection) GetRandom() int {
+
+}
+
+
+/**
+ * Your RandomizedCollection object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Insert(val);
+ * param_2 := obj.Remove(val);
+ * param_3 := obj.GetRandom();
+ */
