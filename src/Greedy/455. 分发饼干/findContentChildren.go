afa747fc70c9b6e54a289092e628adc3038642c9
alexzhou1986 <alexzhou1986@gmail.com>
2020-11-05 14:15:12 +0800

    commit

--- src/Greedy/455. 分发饼干/findContentChildren.go
+++ src/Greedy/455. 分发饼干/findContentChildren.go
@@ -1,47 +1,22 @@
 package _55__分发饼干
 
+import "sort"
+
 func findContentChildren(g []int, s []int) int {
-	quickSort(&g, 0, len(g))
-	quickSort(&s, 0, len(s))
-	//TODO
-	return 0
-}
-
-func quickSort(a *[]int, lo int, hi int) {
-	if hi > lo {
-		j := partition(a, lo, hi)
-		quickSort(a, lo, j-1)
-		quickSort(a, j+1, hi)
-	}
-}
-
-func partition(a *[]int, lo int, hi int) int {
-
-	l := lo
-	r := hi + 1
-	v := (*a)[lo]
-	for true {
-		for (*a)[l]< v {
-			if l==hi {
+	ls := len(s)
+	lg := len(g)
+	sort.Ints(g)
+	sort.Ints(s)
+	i := 0
+	ans := 0
+	for i < ls {
+		if ans == lg {
 			break
-			}
 		}
-		for (*a)[--r] > v{
-			if (r==lo){
-			break
-			}
+		if s[i] >= g[ans] {
+			ans += 1
 		}
-		if l>=r {
-			break //break 要写在交换前面
-		}
-		temp := (*a)[l]
-		(*a)[l] = (*a)[r]
-		(*a)[r] = temp
-
+		i++
 	}
-	//这个时候分割点还在lo位 移动到中间
-	(*a)[lo] = (*a)[r]
-	(*a)[r] = v
-
-	return 0
+	return ans
 }
