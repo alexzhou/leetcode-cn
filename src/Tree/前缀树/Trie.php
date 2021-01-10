580d7aa56b0a0c2071104668f5f09c5c83c96356
alexzhou <alexzhou1986@gmail.com>
2021-01-10 15:00:28 +0800

    commit

--- src/Tree/前缀树/Trie.php
+++ src/Tree/前缀树/Trie.php
@@ -1,17 +1,14 @@
 <?php
-
-
 class Trie {
+    public $root;
     /**
      * Initialize your data structure here.
      */
-
-    public $root;
     function __construct() {
-        $this->root = [
-            'isWord'=>false,
-            'children'=>[]
-        ];
+        $obj = new stdClass();
+        $obj->children = [];
+        $obj->isWord = false;
+        $this->root = $obj;
     }
 
     /**
@@ -21,15 +18,18 @@ class Trie {
      */
     function insert($word) {
         $len = strlen($word);
-        $curr = &$this->root;
-        for($i=0;$i<$len;$i++){
+        $curr = $this->root;
+        for ($i=0;$i<$len;$i++){
             $char = $word[$i];
-            if(!isset($curr['children'][$char])){
-                $curr['children'][$char] = [];
+            if(!isset($curr->children[$char])){
+                $obj = new stdClass();
+                $obj->children = [];
+                $obj->isWord = false;
+                $curr->children[$char] = $obj;
             }
-            $curr = &$curr['children'][$char];
+            $curr = $curr->children[$char];
         }
-        $curr['isWord'] = true;
+        $curr->isWord = true;
     }
 
     /**
@@ -39,15 +39,15 @@ class Trie {
      */
     function search($word) {
         $len = strlen($word);
-        $curr = &$this->root;
-        for($i=0;$i<$len;$i++){
+        $curr = $this->root;
+        for ($i=0;$i<$len;$i++){
             $char = $word[$i];
-            if(!isset($curr['children'][$char])){
+            if(!isset($curr->children[$char])){
                 return false;
             }
-            $curr = &$curr['children'][$char];
+            $curr = $curr->children[$char];
         }
-        return $curr['isWord']===true;
+        return $curr->isWord;
     }
 
     /**
@@ -57,13 +57,13 @@ class Trie {
      */
     function startsWith($prefix) {
         $len = strlen($prefix);
-        $curr = &$this->root;
-        for($i=0;$i<$len;$i++){
+        $curr = $this->root;
+        for ($i=0;$i<$len;$i++){
             $char = $prefix[$i];
-            if(!isset($curr['children'][$char])){
+            if(!isset($curr->children[$char])){
                 return false;
             }
-            $curr = &$curr['children'][$char];
+            $curr = $curr->children[$char];
         }
         return true;
     }
@@ -76,13 +76,3 @@ class Trie {
  * $ret_2 = $obj->search($word);
  * $ret_3 = $obj->startsWith($prefix);
  */
-$obj = new Trie();
-$ret_1 = $obj->insert("apple");
-$ret_2 = $obj->search("apple");
-$ret_3 = $obj->search("app");
-$ret_4 = $obj->startsWith("app");
-$ret_5 = $obj->insert("app");
-$ret_6 = $obj->search("app");
-var_dump($ret_6);
-//var_dump($ret_2);
-//print_r($ret_3);
