f6074c3dca092452a5f5c1c8a5d82a1e126bf38e
alexzhou <alexzhou1986@gmail.com>
2021-01-20 21:22:35 +0800

    LRU php version

--- src/Map/146. LRU缓存机制/LRU.php
+++ src/Map/146. LRU缓存机制/LRU.php
@@ -1,10 +1,31 @@
 <?php
+class Node{
+    public $key;
+    public $val;
+    public $pre;
+    public $next;
+    function __construct($key,$val) {
+        $this->key = $key;
+        $this->val = $val;
+    }
+}
 class LRUCache {
+    private $capacity;
+    private $size;
+    private $head;
+    private $tail;
+    private $cache;
     /**
-     * @param Integer $capacity
+     * @param Integer $capacitys
      */
     function __construct($capacity) {
-
+        $this->capacity = $capacity;
+        $this->size = 0;
+        $this->cache = [];
+        $this->head = new Node(null,null);
+        $this->tail = new Node(null,null);
+        $this->head->next = $this->tail;
+	    $this->tail->pre = $this->head;
     }
 
     /**
@@ -12,7 +33,14 @@ class LRUCache {
      * @return Integer
      */
     function get($key) {
-
+        if(isset($this->cache[$key])){
+            $obj = $this->cache[$key];
+            $this->removeNode($obj);
+            $this->addToHead($obj);
+            return $obj->val;
+        }else{
+            return -1;
+        }
     }
 
     /**
@@ -21,7 +49,35 @@ class LRUCache {
      * @return NULL
      */
     function put($key, $value) {
+        if(!isset($this->cache[$key])){
+            $node = new Node($key,$value);
+            $this->cache[$key] = $node;
+            //add to head
+            $this->addToHead($node);
+            $this->size+=1;
+            if ($this->size > $this->capacity){
+                $old = $this->tail->pre;
+                $this->removeNode($old);
+                unset($this->cache[$old->key]);
+                $this->size-=1;
+            }
+        }else{
+            $node = $this->cache[$key];
+            $node->val = $value;
+            $this->removeNode($node);
+            $this->addToHead($node);
+        }
+    }
+    function removeNode($node){
+        $node->pre->next = $node->next;
+        $node->next->pre = $node->pre;
+    }
 
+    function addToHead($node){
+        $node->pre = $this->head;
+        $node->next = $this->head->next;
+        $this->head->next->pre = $node;
+        $this->head->next = $node;
     }
 }
 
