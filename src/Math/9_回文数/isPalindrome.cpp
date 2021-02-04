8b9b79499d9f980530c33079d12a5f62c1e37131
alexzhou1986 <alexzhou1986@gmail.com>
2021-02-04 16:55:32 +0800

    9

---
+++ src/Math/9_回文数/isPalindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+        return x == revertedNumber || x == revertedNumber / 10;
+    }
+};
+
+int main(){
+    Solution obj;
+    bool ans = obj.isPalindrome(-101);
+    cout<<"ans:"<<ans<<endl;
+    return 0;
+}
