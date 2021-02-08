e5c24a2b6104187c8861bc1ed546070e85637d5b
alexzhou1986 <alexzhou1986@gmail.com>
2021-02-08 14:40:17 +0800

    test

--- src/SlidingWindow/978_最长湍流子数组/maxTurbulenceSize.cpp
+++ src/SlidingWindow/978_最长湍流子数组/maxTurbulenceSize.cpp
@@ -1,35 +1,33 @@
 #include<iostream>
 #include<vector>
-#include <algorithm> 
+#include<algorithm>
 using namespace std;
 class Solution {
 public:
     int maxTurbulenceSize(vector<int>& arr) {
         int n = arr.size();
         int left = 0;
-        int ans = 0;
-        vector<int> list;
-        for(int i=0;i<n-1;i++){
-            int flag = compare(arr[i],arr[i+1]);
-            list.push_back(flag);
-        }
-        for(int j=1;j<list.size();j++){
-            if( list[j-1]+list[j] !=0){
-                ans = max(ans,(j-1)-left+1);
-                left=j;
-            }
-        }
-        return ans+1;
-    }
+        int right = 0;
+        int ans = 1;
 
-    int compare(int a,int b){
-        if(a>b){
-            return 1;
-        }else if(a<b){
-            return -1;
-        }else{
-            return 0;
+        while (right < n - 1) {
+            if (left == right) {
+                if (arr[left] == arr[left + 1]) {
+                    left++;
+                }
+                right++;
+            } else {
+                if (arr[right - 1] < arr[right] && arr[right] > arr[right + 1]) {
+                    right++;
+                } else if (arr[right - 1] > arr[right] && arr[right] < arr[right + 1]) {
+                    right++;
+                } else {
+                    left = right;
+                }
+            }
+            ans = max(ans, right - left + 1);
         }
+        return ans;
     }
 };
 
