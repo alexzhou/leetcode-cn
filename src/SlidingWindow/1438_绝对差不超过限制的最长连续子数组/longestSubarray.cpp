7ccdc607a8fec14fa98d0d0fe089cd283b6327cb
alexzhou <alexzhou1986@gmail.com>
2021-02-22 10:24:59 +0800

    1438

---
+++ src/SlidingWindow/1438_绝对差不超过限制的最长连续子数组/longestSubarray.cpp
@@ -0,0 +1,35 @@
+#include<vector>
+#include<iostream>
+#include<queue>
+using namespace std;
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        deque<int> queMax, queMin;
+        int n = nums.size();
+        int left = 0, right = 0;
+        int ret = 0;
+        while (right < n) {
+            while (!queMax.empty() && queMax.back() < nums[right]) {
+                queMax.pop_back();
+            }
+            while (!queMin.empty() && queMin.back() > nums[right]) {
+                queMin.pop_back();
+            }
+            queMax.push_back(nums[right]);
+            queMin.push_back(nums[right]);
+            while (!queMax.empty() && !queMin.empty() && queMax.front() - queMin.front() > limit) {
+                if (nums[left] == queMin.front()) {
+                    queMin.pop_front();
+                }
+                if (nums[left] == queMax.front()) {
+                    queMax.pop_front();
+                }
+                left++;
+            }
+            ret = max(ret, right - left + 1);
+            right++;
+        }
+        return ret;
+    }
+};
