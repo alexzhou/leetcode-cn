ea5e8b536bb8b9e0c7849b04fe17d32246186f01
alexzhou <alexzhou1986@gmail.com>
2021-02-25 15:55:28 +0800

    867

--- src/Array/867_转置矩阵/transpose.php
+++ src/Array/867_转置矩阵/transpose.php
@@ -8,28 +8,12 @@ class Solution {
     function transpose($matrix) {
         $m = count($matrix);
         $n = count($matrix[0]);
-        $max = max($m,$n);
-        for($i=0;$i<$max;$i++){
-            for($j=0;$j<$max;$j++){
-                if($j>=$i){
-                    continue;
-                }
-                if(isset($matrix[$i][$j]) && isset($matrix[$j][$i])){
-                    $temp = $matrix[$i][$j];
-                    $matrix[$i][$j] = $matrix[$j][$i];
-                    $matrix[$j][$i] = $temp;
-                }elseif(!isset($matrix[$i][$j]) && isset($matrix[$j][$i])){
-                    $matrix[$i][$j] = $matrix[$j][$i];
-                    unset($matrix[$j][$i]);
-                }elseif(isset($matrix[$i][$j]) && !isset($matrix[$j][$i])){
-                    $matrix[$j][$i]  = $matrix[$i][$j];
-                    unset($matrix[$i][$j]);
-                }
-            }
-            if(count($matrix[$i])==0){
-                unset($matrix[$i]);
+        $new = [];
+        for($i=0;$i<$m;$i++){
+            for($j=0;$j<$n;$j++){
+                $new[$j][$i] = $matrix[$i][$j];
             }
         }
-        return $matrix;
+        return $new;
     }
 }
