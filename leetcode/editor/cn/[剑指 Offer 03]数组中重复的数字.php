0bb76058b8c2f374f93cb05c9ffe00f56c86453b
Alex Zhou <alexzhou1986@gmail.com>
2021-03-30 21:22:57 +0800

    offer03

---
+++ leetcode/editor/cn/[剑指 Offer 03]数组中重复的数字.php
@@ -0,0 +1,42 @@
+<?php
+//找出数组中重复的数字。 
+//
+// 
+//在一个长度为 n 的数组 nums 里的所有数字都在 0～n-1 的范围内。数组中某些数字是重复的，但不知道有几个数字重复了，也不知道每个数字重复了几次。请
+//找出数组中任意一个重复的数字。 
+//
+// 示例 1： 
+//
+// 输入：
+//[2, 3, 1, 0, 2, 5, 3]
+//输出：2 或 3 
+// 
+//
+// 
+//
+// 限制： 
+//
+// 2 <= n <= 100000 
+// Related Topics 数组 哈希表 
+// 👍 358 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+
+    /**
+     * @param Integer[] $nums
+     * @return Integer
+     */
+    function findRepeatNumber($nums) {
+        $map = [];
+        foreach ($nums as $num){
+            if(isset($map[$num])){
+                return $num;
+            }else{
+                $map[$num]=1;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
