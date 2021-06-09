67eadad19895a7a1f7ed755f31036166af7ba6ef
alexzhou1986 <alexzhou1986@gmail.com>
2021-06-09 17:11:12 +0800

    523

---
+++ src/Map/前缀和和MAP/525_contiguous-array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        int maxLength = 0;
+        unordered_map<int, int> mp;
+        int counter = 0;
+        mp[counter] = -1;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            //counter 实际上就是前缀和
+            if (num == 1) {
+                counter++;
+            } else {
+                counter--;
+            }
+            if (mp.count(counter)) {
+                int prevIndex = mp[counter];
+                maxLength = max(maxLength, i - prevIndex);
+            } else {
+                mp[counter] = i;
+            }
+        }
+        return maxLength;
+    }
+};
